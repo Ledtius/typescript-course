d1842814a7dae6689eb66b9a629909729bff8de7
Calet Devia <caletdevia@gmail.com>
2025-10-31 21:29:08 -0500

    practice 7 general exercises of ts

--- bases/app.js
+++ bases/app.js
@@ -1,8 +1,56 @@
-var msg = "Hello world";
-console.log(msg);
-var hero = {
-    name: "Robin",
-    age: 19,
+var username;
+var age;
+var isDev;
+var skills;
+function greet(name) {
+    return "Hello ".concat(name.toUpperCase);
+}
+var name = greet("Calet");
+console.log(name);
+function printId(id) {
+    console.log("Your ID is : ".concat(id));
+}
+printId(101);
+printId("abc");
+var user = {
+    name: "Calet",
+    age: 23,
+    isOnline: true,
 };
-hero.age = 54;
-console.log(hero.age);
+// user.name = "3ss♦";
+console.log(user.name);
+function getTotal(numbers) {
+    return numbers.reduce(function (acc, n) { return acc + n; }, 0);
+}
+console.log(getTotal([1, 23, 4, 50]));
+function welcome(name, age) {
+    if (name === void 0) { name = "Guest"; }
+    if (age) {
+        return "Welcome ".concat(name, ", age ").concat(age);
+    }
+    return "Welcome ".concat(name);
+}
+console.log(welcome("Goku", 18));
+var UserRole;
+(function (UserRole) {
+    UserRole["ADMIN"] = "admin";
+    UserRole["EDITOR"] = "editor";
+    UserRole["VIEWER"] = "viewer";
+})(UserRole || (UserRole = {}));
+function checkPermission(role) {
+    switch (role) {
+        case "admin":
+            console.log("Welcome ".concat(role, " - Max level"));
+            break;
+        case "editor":
+            console.log("Welcome ".concat(role, " - Medium level"));
+            break;
+        case "viewer":
+            console.log("Welcome ".concat(role, " - Min level"));
+            break;
+        default:
+            console.log("User no valid");
+            break;
+    }
+}
+checkPermission(UserRole.VIEWER);
--- bases/app.ts
+++ bases/app.ts
@@ -1,12 +1,80 @@
-let msg: string = "Hello world";
+let username: string;
 
-console.log(msg);
+let age: number;
 
-const hero = {
-  name: "Robin",
-  age: 19,
+let isDev: boolean;
+
+let skills: [];
+
+function greet(name: string) {
+  return `Hello ${name.toUpperCase}`;
+}
+
+const name: string = greet("Calet");
+
+console.log(name);
+
+function printId(id: string | number) {
+  console.log(`Your ID is : ${id}`);
+}
+
+printId(101);
+printId("abc");
+
+type User = {
+  name: string;
+  age: number;
+  isOnline: boolean;
 };
 
-hero.age = 54;
+const user: User = {
+  name: "Calet",
+  age: 23,
+  isOnline: true,
+};
 
-console.log(hero.age);
+// user.name = "3ss♦";
+
+console.log(user.name);
+
+function getTotal(numbers: number[]) {
+  return numbers.reduce((acc, n) => acc + n, 0);
+}
+
+console.log(getTotal([1, 23, 4, 50]));
+
+function welcome(name: string = "Guest", age?: number) {
+  if (age) {
+    return `Welcome ${name}, age ${age}`;
+  }
+
+  return `Welcome ${name}`;
+}
+
+console.log(welcome("Goku", 18));
+
+enum UserRole {
+  ADMIN = "admin",
+  EDITOR = "editor",
+  VIEWER = "viewer",
+}
+
+function checkPermission(role: UserRole) {
+  switch (role) {
+    case "admin":
+      console.log(`Welcome ${role} - Max level`);
+      break;
+    case "editor":
+      console.log(`Welcome ${role} - Medium level`);
+      break;
+    case "viewer":
+      console.log(`Welcome ${role} - Min level`);
+      break;
+
+    default:
+      console.log("User no valid");
+      break;
+  }
+}
+
+checkPermission(UserRole.VIEWER);
