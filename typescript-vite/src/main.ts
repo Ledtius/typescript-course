6b1f8c3a1349328d82257093c3b5d4e3b648ebdc
Calet Devia <caletdevia@gmail.com>
2026-01-07 22:01:09 -0500

    Practicing the basics again

--- typescript-vite/src/main.ts
+++ typescript-vite/src/main.ts
@@ -1,14 +1,9 @@
-/* Exe 1 */
+function identity<T>(value: T): T {
+  return value;
+}
 
-const name: string = "Calet";
-let age: number = 24;
-let married: boolean = false;
-let kids: null = null;
-let preferences: undefined = undefined;
+const text = identity("hola");
+const num = identity(10);
 
-/* Exe 2 */
-const add = (a: number, b: number): number => {
-  return a + b;
-};
-
-console.log(add(1, 2));
+text.toUpperCase();
+num.toFixed();
